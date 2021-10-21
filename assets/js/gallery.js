3223f56cba954789f94e4f0cf43b073c4804e93c
Filora T <filorat@outlook.com>
2021-10-20 23:41:26 -0600

    First Commit

---
+++ assets/js/gallery.js
@@ -0,0 +1,17 @@
+const photo = document.querySelector(".container");
+const nicePics = [
+	{
+		id: "1",
+		title: "",
+    pathURL: "",
+    linkURL: "",
+    credit: "",
+    creditURL: "",
+	},
+]
+let picture = "";
+nicePics.forEach(function (item) {
+  picture = picture + `<a href ="${item.linkURL}">
+  <img src="assets/images
+  </a>
+)}
